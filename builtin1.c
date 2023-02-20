89bbf7afcec3313e481be56507edcc6579fafe70
Sonwabiso <fsonwa@gmail.com>
2023-02-20 10:31:00 -0800

    builtin.c

--- builtin1.c
+++ builtin1.c
@@ -1,10 +1,9 @@
 #include "shell.h"
 
 /**
- * _myhistory - displays the history list, one command by line, preceded
- *              with line numbers, starting at 0.
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
+ * _myhistory - displays the history list, one command by line.
+ * @info: Structure containing potential arguments.
+ *
  *  Return: Always 0
  */
 int _myhistory(info_t *info)
@@ -82,9 +81,8 @@ int print_alias(list_t *node)
 
 /**
  * _myalias - mimics the alias builtin (man alias)
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: Always 0
+ * @info: Structure containing potential arguments
+ * Return: Always 0
  */
 int _myalias(info_t *info)
 {
